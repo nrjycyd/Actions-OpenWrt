71944e1a81d323d1647caddd7f4be16079cdcc78
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2023-08-14 07:57:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,6 @@ mkdir -p package/luci-app-diskman && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
 mkdir -p package/parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+
+# Add luci-app-unblockmusic
+git clone https://github.com/maxlicheng/luci-app-unblockmusic.git package/luci-app-unblockmusic
