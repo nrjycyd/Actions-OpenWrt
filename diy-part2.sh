c16407cb583a382c4bbef9484173c6e8887782e8
nrjycyd <77444933+nrjycyd@users.noreply.github.com>
2023-08-15 14:12:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,17 +14,17 @@
 sed -i 's/192.168.1.1/10.0.0.4/g' package/base-files/files/bin/config_generate
 
 # Add luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Add luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # Add luci-app-unblockneteasemusic
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 # Add luci-app-smartdns
-rm -rf feeds/packages/net/smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/packages/net/smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+#rm -rf feeds/luci/applications/luci-app-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
